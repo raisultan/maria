dc7e1bbfc2b662426a9643d7000c2f95b52a0793
raisultan <ki.xbozz@gmail.com>
2022-02-04 18:44:28 +0700

    csh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,22 @@ github_pr_url=`sed -e 's/^"//' -e 's/"$//' <<<"$github_pr_url"`
 
 echo "Looking for diff at ${github_pr_url}"
 curl --request GET --url ${github_pr_url} --header "authorization: Bearer ${GITHUB_TOKEN}" --header "Accept: application/vnd.github.v3.diff" > github_diff.txt
-python remove_deleted.py
+
+rem_del=`cat <<EOF
+def remove_deleted(fn):
+    with open(fn, "rt") as fin:
+        data = fin.read()
+
+    data = data.replace(".py\n+++ /dev/nul", "")
+
+    with open(fn, "wt") as fin:
+        fin.write(data)
+
+remove_deleted("github_diff.txt")
+EOF
+`
+python -c "$rem_del"
+
 python_files=`cat github_diff.txt | grep -E -- "\+\+\+ |\-\-\- " | awk '{print $2}' | grep -Po -- "(?<=[ab]/).+\.py$"`
 echo "Changed files: ${python_files}"
 
